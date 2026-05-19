7d582682bd6eb02fb6e8354f3a0d97f96324ed88
alexmaisa <alexmaisa@proton.me>
2026-05-19 07:32:31 +0700

    chore: add local and tailscale ips to alloweddevorigins in next config

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true, // Required for static export build compatibility
   },
+  allowedDevOrigins: ['192.168.20.11', '100.80.196.99'],
 };
 
 export default nextConfig;
